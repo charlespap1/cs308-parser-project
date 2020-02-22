995cbf1f0476031cf2c1ff64c0ffb09b8e0cac33
Charles Papandreou <cnp20@duke.edu>
2020-02-22 15:22:25 -0500

    Modified Instruction class to fit new design with code superclass

--- src/slogo/model/Instruction.java
+++ src/slogo/model/Instruction.java
@@ -5,33 +5,18 @@ import slogo.State;
 import java.util.List;
 import java.util.Map;
 
-public abstract class Instruction {
+public abstract class Instruction implements Code{
 
-    private int valueForExec;
-    private Instruction possibleInner;
-    private List<Integer> parameters;
+    private List<Code> parameters;
 
     public Instruction(int val){
-        valueForExec = val;
-        possibleInner = null;
         parameters = null;
     }
 
-    public Instruction(Instruction i){
-        valueForExec = -1;
-        possibleInner = i;
-        parameters = null;
-    }
-
-    public abstract int execute(Turtle turtle, List<Variable> vars);
-    public abstract List<String> getNeededVarNames();
+    public abstract int execute(Turtle turtle);
     public abstract int numRequiredArgs();
 
-    protected boolean hasInnerInstruction(){
-        return possibleInner != null;
-    }
-
-    public void setParameters(List<Integer> params){
+    public void setParameters(List<Code> params){
         parameters = params;
     }
 }
