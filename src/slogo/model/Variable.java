ca56405ea850ecc06e74e21e3bdc7aec1e6a6abd
Charles Papandreou <cnp20@duke.edu>
2020-02-22 15:13:15 -0500

    Created Implemenation of Variable as subclass of Code

--- src/slogo/model/Variable.java
+++ src/slogo/model/Variable.java
@@ -1,4 +1,22 @@
 package slogo.model;
 
-public class Variable {
+public class Variable implements Code {
+
+    private int value;
+
+    public Variable(){
+        value = 0;
+    }
+
+    public void setVariable(int var){
+        value = var;
+    }
+
+    public int generateValue(){
+        return value;
+    }
+
+    public String toString(){
+        return "";
+    }
 }
