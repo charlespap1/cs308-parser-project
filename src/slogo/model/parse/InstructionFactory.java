12f32a84e616b12ea4eb975473a2cf0d796a783d
Charles Papandreou <cnp20@duke.edu>
2020-02-22 05:21:55 -0500

    Added functionality for creating new instance from map based on string

--- src/slogo/model/parse/InstructionFactory.java
+++ src/slogo/model/parse/InstructionFactory.java
@@ -1,13 +1,16 @@
 package slogo.model.parse;
 
+import slogo.model.Instruction;
+
+import java.lang.reflect.Constructor;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ClassSelector {
+public class InstructionFactory {
     private RegexHandler keyGrabber;
     private Map<String,Class> mappings;
 
-    public ClassSelector(String language){
+    public InstructionFactory(String language){
         keyGrabber = new RegexHandler();
         mappings = new HashMap<>();
 
@@ -15,5 +18,22 @@ public class ClassSelector {
 
         // general checks, added last
         keyGrabber.addPatterns("Syntax");
+    }
+
+    public Object getSymbolAsObj(String piece) {
+        String objectType = keyGrabber.getSymbol(piece);
+        Object ret = null;
+        try{
+            Class c = mappings.get(objectType);
+            RegexHandler r = new RegexHandler();
+            Constructor objConstruct = c.getDeclaredConstructor();
+            objConstruct.setAccessible(true);
+            ret = objConstruct.newInstance();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            //TODO: relay some message back to UI that code was no bueno.
+        }
+        return ret;
     }
 }
