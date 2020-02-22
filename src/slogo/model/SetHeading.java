a365eb71ca8382c4f89a802b9f9cb67ebb98a565
Michael Xue <myx2@duke.edu>
2020-02-22 17:44:20 -0500

    Modified classes to new code structure

--- src/slogo/model/SetHeading.java
+++ src/slogo/model/SetHeading.java
@@ -7,13 +7,14 @@ public class SetHeading extends Instruction {
     private static final int numArgs = 1;
 
     @Override
-    public double execute (Turtle t, List<Variable> vars) {
+    public int execute (Turtle t) {
 //        if (this.hasInnerInstruction()) {
 //            this.valueForExec = this.possibleInner.execute(t, vars);
 //        }
 //        double prevAngle = t.getAngle();
 //        t.setAngle(this.valueForExec);
 //        return Math.abs(t.getAngle() - prevAngle);
+        return 0;
     }
 
     public int numRequiredArgs(){
--- src/slogo/model/SetHome.java
+++ src/slogo/model/SetHome.java
@@ -7,7 +7,7 @@ public class SetHome extends Instruction {
     private static final int numArgs = 1;
 
     @Override
-    public double execute (Turtle t, List<Variable> vars) {
+    public int execute (Turtle t) {
 //        if (this.hasInnerInstruction()) {
 //            this.valueForExec = this.possibleInner.execute(t, vars);
 //        }
