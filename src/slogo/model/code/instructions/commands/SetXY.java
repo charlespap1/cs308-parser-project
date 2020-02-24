01aaa0517f16c170b619ecee510806c5862db2f8
Charles Papandreou <cnp20@duke.edu>
2020-02-23 23:46:48 -0500

    added constructor and implemented execute

--- src/slogo/model/code/instructions/commands/SetXY.java
+++ src/slogo/model/code/instructions/commands/SetXY.java
@@ -4,34 +4,37 @@ import slogo.model.Turtle;
 import slogo.model.code.Token;
 import slogo.model.code.instructions.Instruction;
 
+import java.util.List;
+
 public class SetXY extends Instruction {
 
     private static final int NUM_ARGS = 2;
 
+    public SetXY(String name){
+        super();
+        this.instrName = name;
+    }
+
     @Override
     public void execute (Turtle t) {
-        Token xCoordToRelocate = this.parameters.get(0);
-        Token yCoordToRelocate = this.parameters.get(1);
-        if (xCoordToRelocate instanceof Instruction) {
-            ((Instruction) xCoordToRelocate).execute(t);
-        }
-        if (yCoordToRelocate instanceof Instruction) {
-            ((Instruction) yCoordToRelocate).execute(t);
-        }
-        int xCoord = xCoordToRelocate.generateValue();
-        int yCoord = yCoordToRelocate.generateValue();
-        t.setLocation(xCoord, yCoord);
+        List<Integer> paramsAsInts = getParamsAsInts(t);
+        int xCord = paramsAsInts.get(0);
+        int yCord = paramsAsInts.get(1);
+        this.valueOfExecution = (int)distFrom(xCord,yCord,t.getXPos(),t.getYPos());
+        t.setLocation(xCord, yCord);
+        t.setCurrCommand(toString(xCord + "",yCord + ""));
+        t.setCurrCommand("");
     }
 
     public int numRequiredArgs(){
         return NUM_ARGS;
     }
 
-    public int generateValue(){
-        return this.parameters.get(0).generateValue();
+    public String toString(){
+        return instrName;
     }
 
-    public String toString(){
-        return "";
+    public String toString(String x, String y){
+        return toString() + " " + x + "," + y;
     }
 }
