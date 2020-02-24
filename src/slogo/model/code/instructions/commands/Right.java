0620876d360c93fcefcf733b40673c418680c868
Charles Papandreou <cnp20@duke.edu>
2020-02-24 06:54:28 -0500

    Modified forward back left right so orientation of screen for forward is up with 0degrees to right

--- src/slogo/model/code/instructions/commands/Right.java
+++ src/slogo/model/code/instructions/commands/Right.java
@@ -20,7 +20,7 @@ public class Right extends Instruction {
         List<Integer> paramsAsVals = getParamsAsVals(t);
         int valueForExec = paramsAsVals.get(0);
         this.valueOfExecution = valueForExec;
-        double angle = t.getAngle() - valueForExec;
+        double angle = t.getAngle() + valueForExec;
         t.setAngle(angle);
         t.setCurrCommand(toString());
         t.setCurrCommand("");
