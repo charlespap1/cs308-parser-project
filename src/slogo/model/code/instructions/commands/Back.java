c4c784ae8daef8c789310fea2060be4629f2575e
Charles Papandreou <cnp20@duke.edu>
2020-02-23 22:44:27 -0500

    modified constructor and generatevalue handling in Back

--- src/slogo/model/code/instructions/commands/Back.java
+++ src/slogo/model/code/instructions/commands/Back.java
@@ -8,24 +8,28 @@ public class Back extends Instruction {
 
     private static final int numArgs = 1;
 
+    public Back(String name){
+        super();
+        this.instrName = name;
+    }
+
     @Override
-    public void execute (Turtle t) { //(Turtle t, Collection<Variable> vars)
+    public void execute (Turtle t) {
         Token valueToMove = this.parameters.get(0);
         if (valueToMove instanceof Instruction) {
             ((Instruction) valueToMove).execute(t);
         }
         int valueForExec = valueToMove.generateValue();
+        this.valueOfExecution = valueForExec;
         double x = t.getXPos() - valueForExec * Math.cos(t.getAngle());
         double y = t.getYPos() - valueForExec * Math.sin(t.getAngle());
         t.setLocation(x, y);
+        t.setCurrCommand("forward " +valueForExec);
+        t.setCurrCommand("");
     }
 
     public int numRequiredArgs(){
         return numArgs;
-    }
-
-    public int generateValue(){
-        return this.parameters.get(0).generateValue();
     }
 
     public String toString(){
