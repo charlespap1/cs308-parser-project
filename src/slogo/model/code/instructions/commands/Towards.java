0924a7f73e0861e7e918f1e75cf12cc1de0e41e3
Charles Papandreou <cnp20@duke.edu>
2020-02-24 00:01:59 -0500

    Towards methods implemented

--- src/slogo/model/code/instructions/commands/Towards.java
+++ src/slogo/model/code/instructions/commands/Towards.java
@@ -4,35 +4,38 @@ import slogo.model.Turtle;
 import slogo.model.code.Token;
 import slogo.model.code.instructions.Instruction;
 
+import java.util.List;
+
 public class Towards extends Instruction {
 
     private static final int NUM_ARGS = 2;
 
+    public Towards(String name){
+        super();
+        this.instrName = name;
+    }
+
     @Override
     public void execute (Turtle t) {
-        Token xCoordToFace = this.parameters.get(0);
-        Token yCoordToFace = this.parameters.get(1);
-        if (xCoordToFace instanceof Instruction) {
-            ((Instruction) xCoordToFace).execute(t);
-        }
-        if (yCoordToFace instanceof Instruction) {
-            ((Instruction) yCoordToFace).execute(t);
-        }
-        int xCoord = xCoordToFace.generateValue();
-        int yCoord = yCoordToFace.generateValue();
-        double angle = Math.atan2(yCoord - t.getYPos(), xCoord - t.getXPos());
+        List<Integer> paramsAsInts = getParamsAsVals(t);
+        int xCord = paramsAsInts.get(0);
+        int yCord = paramsAsInts.get(1);
+        double angle = Math.atan2(yCord - t.getYPos(), xCord - t.getXPos());
+        this.valueOfExecution = (int)(Math.abs(t.getAngle() - angle));
         t.setAngle(angle);
+        t.setCurrCommand(toString(xCord + "", yCord + ""));
+        t.setCurrCommand("");
     }
 
     public int numRequiredArgs(){
         return NUM_ARGS;
     }
 
-    public int generateValue(){
-        return this.parameters.get(0).generateValue();
+    public String toString(){
+        return instrName;
     }
 
-    public String toString(){
-        return "";
+    public String toString(String x, String y){
+        return toString() + " " + x + "," + y;
     }
 }
