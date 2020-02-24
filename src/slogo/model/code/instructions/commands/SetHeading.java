d2b12ce119167d49e238bd93c520bdb30b5a82b7
Charles Papandreou <cnp20@duke.edu>
2020-02-23 23:24:04 -0500

    modified methods

--- src/slogo/model/code/instructions/commands/SetHeading.java
+++ src/slogo/model/code/instructions/commands/SetHeading.java
@@ -8,26 +8,33 @@ public class SetHeading extends Instruction {
 
     private static final int numArgs = 1;
 
+    public SetHeading(String name){
+        super();
+        this.instrName = name;
+    }
+
     @Override
-    public void execute (Turtle t) { //(Turtle t, Collection<Variable> vars)
+    public void execute (Turtle t) {
         Token angleValue = this.parameters.get(0);
         if (angleValue instanceof Instruction) {
             ((Instruction) angleValue).execute(t);
         }
         int valueForExec = angleValue.generateValue();
+        this.valueOfExecution = (int)Math.abs(t.getAngle() - valueForExec);
         t.setAngle(valueForExec);
+        t.setCurrCommand(toString(valueForExec + ""));
+        t.setCurrCommand("");
     }
 
     public int numRequiredArgs(){
         return numArgs;
     }
 
-    public int generateValue(){
-        return this.parameters.get(0).generateValue();
-    }
-    //TODO: needs to return the change in angle
-
     public String toString(){
-        return "";
+        return instrName;
+    }
+
+    public String toString(String heading){
+        return toString() + " " + heading;
     }
 }
