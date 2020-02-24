e29c967ccc7eb884c7bd303ee45b24a977285d62
Charles Papandreou <cnp20@duke.edu>
2020-02-23 22:27:49 -0500

    Modified instruction to have an implementation of generateValue with private variable

--- src/slogo/model/code/instructions/Instruction.java
+++ src/slogo/model/code/instructions/Instruction.java
@@ -10,13 +10,20 @@ public abstract class Instruction implements Token {
 
     protected List<Token> parameters;
     protected String instrName;
+    protected int valueOfExecution;
 
     public Instruction(){
         parameters = null;
+        instrName = "";
+        valueOfExecution = 0;
     }
 
     public abstract void execute(Turtle turtle);
     public abstract int numRequiredArgs();
+
+    public int generateValue() {
+        return valueOfExecution;
+    }
 
     public void setParameters(List<Token> params){
         parameters = params;
