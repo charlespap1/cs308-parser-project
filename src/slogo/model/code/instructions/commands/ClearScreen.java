0bfc5f541a534a4b7ac0488626adf921f2255e28
Charles Papandreou <cnp20@duke.edu>
2020-02-23 22:52:08 -0500

    implemented constructor execute and removed generateVal for ClearScreen

--- src/slogo/model/code/instructions/commands/ClearScreen.java
+++ src/slogo/model/code/instructions/commands/ClearScreen.java
@@ -10,19 +10,25 @@ public class ClearScreen extends Instruction {
     public static final int HOME_Y = 0;
     private static final int NUM_ARGS = 0;
 
+    public ClearScreen(String name){
+        super();
+        this.instrName = name;
+    }
+
     @Override
     public void execute (Turtle t) {
         //TODO: how will frontend know when this instruction is called and erase the turtle's trails?
+        double currX = t.getXPos();
+        double currY = t.getYPos();
+        double distToHome = Math.sqrt(Math.pow(currX - HOME_X,2) + Math.pow(currY - HOME_Y,2));
+        this.valueOfExecution = (int)distToHome;
         t.setLocation(HOME_X, HOME_Y);
+        t.setCurrCommand("Cleared Screen");
+        t.setCurrCommand("");
     }
 
     public int numRequiredArgs(){
         return NUM_ARGS;
-    }
-
-    //TODO: needs to return the distance turtle moved
-    public int generateValue(){
-        return this.parameters.get(0).generateValue();
     }
 
     public String toString(){
