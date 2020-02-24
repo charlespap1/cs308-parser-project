432a0f6daeca0befae08870a593bcce046c6d251
Charles Papandreou <cnp20@duke.edu>
2020-02-23 23:04:01 -0500

    implemented execute and constructor

--- src/slogo/model/code/instructions/commands/PenUp.java
+++ src/slogo/model/code/instructions/commands/PenUp.java
@@ -7,22 +7,24 @@ public class PenUp extends Instruction {
 
     private static final int numArgs = 0;
 
+    public PenUp(String name){
+        super();
+        this.instrName = name;
+    }
+
     @Override
-    public void execute (Turtle t) { //(Turtle t, Collection<Variable> vars)
-        //t.setProperties(t.getXPos(), t.getYPos(), t.getAngle(), true);
+    public void execute (Turtle t) {
         t.setPenUp(true);
+        this.valueOfExecution = 0;
+        t.setCurrCommand("Pen Up");
+        t.setCurrCommand("");
     }
 
     public int numRequiredArgs(){
         return numArgs;
     }
 
-    public int generateValue(){
-        return 0;
-    }
-
     public String toString(){
         return "Pen Up";
     }
-    // TODO: why is this here? should be from resource file
 }
