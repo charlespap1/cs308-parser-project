78c33dc7000121510d67d474bd2f5a705fd50433
Charles Papandreou <cnp20@duke.edu>
2020-02-24 17:50:15 -0500

    Modified and added code to allow for bracket handling in stacks

---
+++ src/slogo/model/code/BracketClose.java
@@ -0,0 +1,13 @@
+package slogo.model.code;
+
+public class BracketClose implements Token {
+
+    public BracketClose(String name){
+        super();
+    }
+
+    @Override
+    public int generateValue() {
+        return 0;
+    }
+}
---
+++ src/slogo/model/code/BracketOpen.java
@@ -0,0 +1,13 @@
+package slogo.model.code;
+
+public class BracketOpen implements Token {
+
+    public BracketOpen(String name){
+        super();
+    }
+
+    @Override
+    public int generateValue() {
+        return 0;
+    }
+}
