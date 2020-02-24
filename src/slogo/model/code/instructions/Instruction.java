fa0efc0c4278fbc4f3005f72891915d534f70a6a
Charles Papandreou <cnp20@duke.edu>
2020-02-23 23:45:53 -0500

    Added function to execute parameters if needed and then get all values of parameters

--- src/slogo/model/code/instructions/Instruction.java
+++ src/slogo/model/code/instructions/Instruction.java
@@ -3,6 +3,7 @@ package slogo.model.code.instructions;
 import slogo.model.Turtle;
 import slogo.model.code.Token;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -31,6 +32,17 @@ public abstract class Instruction implements Token {
 
     protected double distFrom(double x, double y, double x2, double y2){
         return Math.sqrt(Math.pow(x2 - x,2) + Math.pow(y2 - y,2));
+    }
+
+    protected List<Integer> getParamsAsInts(Turtle t){
+        List<Integer> paramsAsInts = new ArrayList<>();
+        for(Token currToken: parameters){
+            if(currToken instanceof Instruction){
+                ((Instruction) currToken).execute(t);
+            }
+            paramsAsInts.add(currToken.generateValue());
+        }
+        return paramsAsInts;
     }
 
 }
