7761ea40d12f8703c77601a2b802f801f2a8d71f
natalie.novitsky <nen4@duke.edu>
2020-02-26 16:52:21 -0500

    refactoring model (parse and general pkgs)

--- src/slogo/model/ModelAPI.java
+++ src/slogo/model/ModelAPI.java
@@ -1,12 +1,15 @@
 package slogo.model;
 
+import javafx.beans.property.StringProperty;
+import javafx.collections.ObservableList;
+
 import java.io.File;
 
 public interface ModelAPI {
-
     void executeCode(String rawCode);
-
     void executeCode(File f);
-
     Turtle getTurtle();
+    ObservableList<String> getVariableList();
+    ObservableList<String> getNewCommandsList();
+    StringProperty getErrorMessage();
 }
