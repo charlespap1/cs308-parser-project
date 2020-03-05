c6349f753cb3135f3c1068a43b0b1d90e065bb3c
Natalie Novitsky <natalie.novitsky@duke.edu>
2020-03-05 00:52:01 -0500

    Revert "fixing merge conflicts"
    This reverts commit e1626d37176116bffe511faecac291f07ad655aa

--- src/slogo/controller/Controller.java
+++ src/slogo/controller/Controller.java
@@ -68,10 +68,6 @@ public class Controller extends Application {
         setupCommands(myView, myModel);
         myModel.setAddTurtleFunction(myView::addTurtle);
         myView.setPopupButton(e -> showPopUp(stage, myModel));
-        //TODO: add listener for method tell command
-        //myView.add(turtle);
-        myView.setUndoAction(e -> myModel.undo());
-        myView.setRedoAction(e -> myModel.redo());
     }
 
     private void getNewPreferences(Stage currentStage) {
