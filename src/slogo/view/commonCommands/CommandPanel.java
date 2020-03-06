3c1173ad29694a29baf2995c70828e27fcc45379
Juliet Yznaga <jay18@duke.edu>
2020-03-05 21:26:48 -0500

    Consolidated commands

--- src/slogo/view/commonCommands/CommandPanel.java
+++ src/slogo/view/commonCommands/CommandPanel.java
@@ -1,9 +1,14 @@
 package slogo.view.commonCommands;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.beans.property.StringProperty;
 import javafx.scene.Node;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import slogo.view.SetupScreen;
@@ -13,25 +18,30 @@ import slogo.view.StaticViewElement;
  * Provides generic way to put common phrases into boxes for displaying
  * @author Juliet
  */
-public abstract class CommandPanel implements StaticViewElement {
+public class CommandPanel implements StaticViewElement {
   public static final int HOLDER_PADDING = 10;
+  public static final int SCROLLER_HEIGHT = SetupScreen.WIDTH - 400;
+  public static final int NUM_PANELS = 4;
+  public static final String TAB = "     ";
   public static final String RESOURCES = "resources";
   public static final String RESOURCE_PACKAGE = RESOURCES + ".commands.";
   public static final int VBOX_SPACING = 10;
 
   private ResourceBundle myResources;
   private VBox myHolder;
-  private List<String> myIdentifiers;
   private Text myTitle = new Text();
+  private ScrollPane myView = new ScrollPane();
+  private List<String> myIdentifiers;
 
-  public CommandPanel(String resourceTextFile, List<String> identifiers, double x, double y) {
+  public CommandPanel(String resourceTextFile, String [] identifiers, double x, double y) {
     String myPackage = RESOURCE_PACKAGE + resourceTextFile;
     myResources = ResourceBundle.getBundle(myPackage);
-    myIdentifiers = identifiers;
 
+    myIdentifiers = new ArrayList<>(Arrays.asList(identifiers));
     myHolder = new VBox(VBOX_SPACING);
     setHolderLayout(x, y);
     fillCommands();
+    myHolder.getChildren().add(myView);
   }
 
   public Node getView()
@@ -46,16 +56,24 @@ public abstract class CommandPanel implements StaticViewElement {
   }
 
   private void setHolderLayout(double x, double y) {
-    myHolder.setMinWidth(SetupScreen.WIDTH/4.0);
-    myHolder.setMaxWidth(SetupScreen.WIDTH/4.0);
+    myHolder.setMinWidth(SetupScreen.WIDTH/NUM_PANELS - HOLDER_PADDING);
+    myHolder.setMaxWidth(SetupScreen.WIDTH/NUM_PANELS- HOLDER_PADDING);
+    myHolder.setMaxHeight(SCROLLER_HEIGHT);
     myHolder.setLayoutX(x + HOLDER_PADDING);
     myHolder.setLayoutY(y + HOLDER_PADDING);
   }
 
   private void fillCommands() {
+    VBox textHolder = new VBox(VBOX_SPACING);
+    Text topLine = new Text();
+    textHolder.getChildren().add(topLine);
+
     for(String comm: myIdentifiers){
       String command = myResources.getString(comm);
-      myHolder.getChildren().add(new Text(command));
+      textHolder.getChildren().add(new Text(TAB+ command));
     }
+    Text bottomLine = new Text();
+    textHolder.getChildren().add(bottomLine);
+    myView.setContent(textHolder);
   }
 }
