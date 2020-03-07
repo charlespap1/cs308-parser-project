c62d58ac1981f55a7ca1d813e284b46970c01896
Juliet Yznaga <jay18@duke.edu>
2020-03-06 22:32:30 -0600

    Making preferences more user friendly

--- src/slogo/view/popup/SetPreferencesPopup.java
+++ src/slogo/view/popup/SetPreferencesPopup.java
@@ -1,5 +1,9 @@
 package slogo.view.popup;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ComboBox;
@@ -13,12 +17,16 @@ public class SetPreferencesPopup extends PopupSkeleton implements ViewPopup {
 
     private static final int LIST_WIDTH = 165;
     private static final int LIST_HEIGHT = 20;
+    private static final String RESOURCE_PACKAGE = "resources.preferences.PreferenceMap";
+    private static final String DEFAULT_KEY = "DefaultPreferences";
+    private ResourceBundle myPreferenceMap = ResourceBundle.getBundle(RESOURCE_PACKAGE);
+    private Map<String, String> myPreferenceTranslations;
+
 
     private ComboBox<String> myList;
 
     public SetPreferencesPopup() {
         super();
-        myPrompt.setText("    Choose \n Preference: ");
         setList();
         myList.setPrefSize(LIST_WIDTH, LIST_HEIGHT);
         myHolder.getChildren().add(2, myList);
@@ -31,13 +39,35 @@ public class SetPreferencesPopup extends PopupSkeleton implements ViewPopup {
      * @return
      */
     public String getPreference() {
-        return myList.getSelectionModel().getSelectedItem();
+        return getKey(myList.getSelectionModel().getSelectedItem());
     }
 
     private void setList() {
         myList = new ComboBox<>();
-        ObservableList<String> items = FXCollections.observableArrayList("DefaultPreferences", "PreferencesOne");
-        myList.setValue("DefaultPreferences");
+        myPreferenceTranslations = new HashMap<>();
+
+        ObservableList<String> items = FXCollections.observableArrayList();
+        for(String key: myPreferenceMap.keySet())
+        {
+            String visualTranslation = myPreferenceMap.getString(key);
+            myPreferenceTranslations.put(key, visualTranslation);
+            items.add(visualTranslation);
+        }
+
+        myList.setValue(myPreferenceMap.getString(DEFAULT_KEY));
         myList.setItems(items);
     }
+
+    private String getKey(String value)
+    {
+        for(String key: myPreferenceMap.keySet())
+        {
+            if(myPreferenceMap.getString(key) == value)
+            {
+                return key;
+            }
+        }
+        return DEFAULT_KEY;
+    }
+
 }
